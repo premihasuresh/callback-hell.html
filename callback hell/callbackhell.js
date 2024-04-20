7d3349e87b096edd57dadd6916b11ac5c36c2098
premihasuresh <163987455+premihasuresh@users.noreply.github.com>
2024-04-20 20:25:22 +0530

    Add files via upload

---
+++ callback hell/callbackhell.js
@@ -0,0 +1,38 @@
+var div = document.getElementById("cbhell")
+
+setTimeout(() =>{
+    div.innerHTML='10'
+    setTimeout(()=>{
+        div.innerHTML='9'
+        setTimeout(()=>{
+            div.innerHTML='8'
+            setTimeout(()=>{
+                div.innerHTML='7'
+               setTimeout(()=>{
+                    div.innerHTML='6'
+                setTimeout(()=>{
+                    div.innerHTML='5'
+                    setTimeout(()=>{
+                        div.innerHTML='4'
+                        setTimeout(()=>{
+                            div.innerHTML='3'
+                         setTimeout(()=>{
+                                div.innerHTML='2'
+                               setTimeout(()=>{
+                                    div.innerHTML='1'
+                                   setTimeout(()=>{
+                                        div.innerHTML='0'
+                                        setTimeout(()=>{
+                                            div.innerHTML='Happy Independance Day'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                 },1000)
+                 },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
